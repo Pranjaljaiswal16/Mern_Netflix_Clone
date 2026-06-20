dc24f1d75c0007f2c5cd9836f98a120be99474b6
Pranjaljaiswal0416 <er.pranjaljaiswal@gmail.com>
2026-06-20 22:53:20 +0530

    Database

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,12 +1,11 @@
-import mongoose from "mongoose";
-
 const connectDB = async () => {
   try {
+    console.log("MONGODB_URI:", process.env.MONGODB_URI);
+
     await mongoose.connect(process.env.MONGODB_URI);
+
     console.log("MONGODB CONNECTED");
   } catch (error) {
     console.log("error", error);
   }
 };
-
-export default connectDB;
